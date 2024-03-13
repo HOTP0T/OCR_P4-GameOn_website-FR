528df90255458a0391afe7ee7c412f763af438ef
HOTPOT <tpcmaky@gmail.com>
2024-03-13 22:38:08 +0100

    fixed handling of error messages on form.js, added the correct error messages in html instead of placeholders, only KAN-7 remains

--- scripts.js/form.js
+++ scripts.js/form.js
@@ -1,89 +1,185 @@
-// empeche le default behaviour du formulaire lors du submit en cas derreur -> il ne refraichit pas la page
-let formulaire = document.getElementById("formulaire");
-formulaire.addEventListener("submit", function (e) {
-  e.preventDefault();
-  launchValidation();
-}); 
+document
+  .querySelector("#formulaire")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-// verification contenu formulaire
-// function validateInput() {
-//   var fname = document.getElementById("first").value; //retrieve first name value
-//   var lname = document.getElementById("last").value; //retrieve last name value
-//   var email = document.getElementById("email").value; //retrieve email value
-//   var emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i; //regex for email
-//   var birthday = document.getElementById("birthdate").value; //retrieve birthdate value
-//   var tourQuantity = document.getElementById("quantity").value; //retrieve tournament quantity value
-//   var location = document.querySelectorAll('input[type="radio"]:checked'); //retrieve location
+    var errors = false;
 
-//   // check first name
-//   if (fname == "" || fname.length < 2 || fname.search(/^[a-zA-Z]+$/) == -1) {
-//     alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
-//     console.log("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
-//     // document.querySelector("#first").style.border = "2px solid red";
-//     // document.querySelector(".errorFirst").style.display = "block";
-//   }
-//   // check last name
-//   else if (
-//     lname == "" ||
-//     lname.length < 2 ||
-//     lname.search(/^[a-zA-Z]+$/) == -1
-//   ) {
-//     alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
-//     console.log("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
-//     // document.querySelector("#last").style.border = "2px solid red";
-//     // document.querySelector(".errorLast").style.display = "block";
-//   }
-//   //check email
-//   else if (email == "") {
-//     alert("Veuillez entrer une adresse email valide.");
-//     console.log("Please Enter Email");
-//     // document.querySelector("#email").style.border = "2px solid red";
-//     // document.querySelector(".errorEmail").style.display = "block";
-//   } else if (email.search(emailRegEx) == -1) {
-//     alert("Veuillez entrer une adresse email valide.");
-//     console.log("Please enter a valid email address.");
-//     // document.querySelector("#email").style.border = "2px solid red";
-//     // document.querySelector(".errorEmail").style.display = "block";
-//   }
-//   // check birthdate
-//   else if (!birthday) {
-//     alert("Vous devez entrer votre date de naissance.");
-//     console.log("Please select Birth Date");
-//     // document.querySelector("#birthdate").style.border = "2px solid red";
-//     // document.querySelector(".errorBOD").style.display = "block";
-//   }
-//   // check location radiobuttons
-//   else if (location.length === 0) {
+    var first = document.querySelector("#first");
+    var last = document.querySelector("#last");
+    var email = document.querySelector("#email");
+    var birthdate = document.querySelector("#birthdate");
+    var quantity = document.querySelector("#quantity");
+    var location = document.querySelectorAll('input[name="location"]');
+    var locationChecked = false;
+    var termsCheckbox = document.querySelector("#checkbox1");
+
+    if (first.value.trim().length < 2) {
+      document.querySelector(".errorFirst").style.display = "inline";
+      errors = true;
+    } else {
+      document.querySelector(".errorFirst").style.display = "none";
+    }
+
+    if (last.value.trim().length < 2) {
+      document.querySelector(".errorLast").style.display = "inline";
+      errors = true;
+    } else {
+      document.querySelector(".errorLast").style.display = "none";
+    }
+
+    if (email.value.trim().length < 2 || !email.validity.valid) {
+      document.querySelector(".errorEmail").style.display = "inline";
+      errors = true;
+    } else {
+      document.querySelector(".errorEmail").style.display = "none";
+    }
+
+    if (birthdate.value.trim().length < 2) {
+      document.querySelector(".errorBOD").style.display = "inline";
+      errors = true;
+    } else {
+      document.querySelector(".errorBOD").style.display = "none";
+    }
+
+    if (
+      quantity.value.trim().length < 1 ||
+      isNaN(quantity.value) ||
+      quantity.value < 0 ||
+      quantity.value > 99
+    ) {
+      document.querySelector(".errorQuantity").style.display = "inline";
+      errors = true;
+    } else {
+      document.querySelector(".errorQuantity").style.display = "none";
+    }
+
+    for (var i = 0; i < location.length; i++) {
+      if (location[i].checked) {
+        locationChecked = true;
+      }
+    }
+
+    if (!locationChecked) {
+      document.querySelector(".errorLocation").style.display = "inline";
+      errors = true;
+    } else {
+      document.querySelector(".errorLocation").style.display = "none";
+    }
+
+if (!termsCheckbox.checked) {
+  document.querySelector(".errorCheckbox").style.display = "block";
+  errors = true;
+} else {
+  document.querySelector(".errorCheckbox").style.display = "none";
+}
+
+    if (!errors) {
+      launchValidation();
+    }
+
+    return !errors;
+  });
+
+// // empeche le default behaviour du formulaire lors du submit en cas derreur -> il ne refraichit pas la page
+// let formulaire = document.getElementById("formulaire");
+// formulaire.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   checkLocation();
+//   launchValidation();
+// });
+
+// // check location radio button
+// function checkLocation() {
+//   const location = document.querySelectorAll('input[type="radio"]:checked'); //retrieve location
+//   const errorLocation = document.querySelector(".errorLocation");
+//   if (location.length === 0) {
+//     errorLocation.style.display = "block";
 //     alert("Vous devez choisir une option.");
 //     // console.log("Please select at least one location");
 //     // document.querySelector(".errorLocation").style.display = "block";
 //   }
-//   // check tournament quantity
-//   else if (tourQuantity == "") {
-//     alert("Please enter a number");
-//     console.log("Please enter a number");
-//     // document.querySelector("#quantity").style.border = "2px solid red";
-//     // document.querySelector(".errorQuantity").style.display = "block";
-//   } else if (tourQuantity < 0) {
-//     alert("Please enter a positive number");
-//     console.log("Please enter a positive number");
-//     // document.querySelector("#quantity").style.border = "2px solid red";
-//     // document.querySelector(".errorQuantity").style.display = "block";
-//   } else if (tourQuantity > 99) {
-//     alert("Please enter a number between 0 and 99");
-//     console.log("Please enter a number between 0 and 99");
-//     // document.querySelector("#quantity").style.border = "2px solid red";
-//     // document.querySelector(".errorQuantity").style.display = "block";
-//   }
-//   // check terms of use
-//   else if (document.getElementById("checkbox1").checked == false) {
-//     alert("You must accept the terms of use");
-//     console.log("You must accept the terms of use");
-//   }
-//   // closes the form if all fields are filled
-//   else {
-//     alert("Form Submitted Successfully");
-//     console.log("Form Submitted Successfully");
-//     launchValidation();
-//   }
 // }
+
+// // verification contenu formulaire
+// // function validateInput() {
+// //   var fname = document.getElementById("first").value; //retrieve first name value
+// //   var lname = document.getElementById("last").value; //retrieve last name value
+// //   var email = document.getElementById("email").value; //retrieve email value
+// //   var emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i; //regex for email
+// //   var birthday = document.getElementById("birthdate").value; //retrieve birthdate value
+// //   var tourQuantity = document.getElementById("quantity").value; //retrieve tournament quantity value
+// //   var location = document.querySelectorAll('input[type="radio"]:checked'); //retrieve location
+
+// //   // check first name
+// //   if (fname == "" || fname.length < 2 || fname.search(/^[a-zA-Z]+$/) == -1) {
+// //     alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+// //     console.log("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+// //     // document.querySelector("#first").style.border = "2px solid red";
+// //     // document.querySelector(".errorFirst").style.display = "block";
+// //   }
+// //   // check last name
+// //   else if (
+// //     lname == "" ||
+// //     lname.length < 2 ||
+// //     lname.search(/^[a-zA-Z]+$/) == -1
+// //   ) {
+// //     alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+// //     console.log("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+// //     // document.querySelector("#last").style.border = "2px solid red";
+// //     // document.querySelector(".errorLast").style.display = "block";
+// //   }
+// //   //check email
+// //   else if (email == "") {
+// //     alert("Veuillez entrer une adresse email valide.");
+// //     console.log("Please Enter Email");
+// //     // document.querySelector("#email").style.border = "2px solid red";
+// //     // document.querySelector(".errorEmail").style.display = "block";
+// //   } else if (email.search(emailRegEx) == -1) {
+// //     alert("Veuillez entrer une adresse email valide.");
+// //     console.log("Please enter a valid email address.");
+// //     // document.querySelector("#email").style.border = "2px solid red";
+// //     // document.querySelector(".errorEmail").style.display = "block";
+// //   }
+// //   // check birthdate
+// //   else if (!birthday) {
+// //     alert("Vous devez entrer votre date de naissance.");
+// //     console.log("Please select Birth Date");
+// //     // document.querySelector("#birthdate").style.border = "2px solid red";
+// //     // document.querySelector(".errorBOD").style.display = "block";
+// //   }
+// //   // check location radiobuttons
+// //   else if (location.length === 0) {
+// //     alert("Vous devez choisir une option.");
+// //     // console.log("Please select at least one location");
+// //     // document.querySelector(".errorLocation").style.display = "block";
+// //   }
+// //   // check tournament quantity
+// //   else if (tourQuantity == "") {
+// //     alert("Please enter a number");
+// //     console.log("Please enter a number");
+// //     // document.querySelector("#quantity").style.border = "2px solid red";
+// //     // document.querySelector(".errorQuantity").style.display = "block";
+// //   } else if (tourQuantity < 0) {
+// //     alert("Please enter a positive number");
+// //     console.log("Please enter a positive number");
+// //     // document.querySelector("#quantity").style.border = "2px solid red";
+// //     // document.querySelector(".errorQuantity").style.display = "block";
+// //   } else if (tourQuantity > 99) {
+// //     alert("Please enter a number between 0 and 99");
+// //     console.log("Please enter a number between 0 and 99");
+// //     // document.querySelector("#quantity").style.border = "2px solid red";
+// //     // document.querySelector(".errorQuantity").style.display = "block";
+// //   }
+// //   // check terms of use
+// //   else if (document.getElementById("checkbox1").checked == false) {
+// //     alert("You must accept the terms of use");
+// //     console.log("You must accept the terms of use");
+// //   }
+// //   // closes the form if all fields are filled
+// //   else {
+// //     alert("Form Submitted Successfully");
+// //     console.log("Form Submitted Successfully");
+// //     launchValidation();
+// //   }
+// // }
